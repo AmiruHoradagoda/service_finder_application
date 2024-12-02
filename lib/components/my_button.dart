e20cd40049bf095d6c0caa830f6dcfc50d106ff4
AmiruHoradagoda <139224955+AmiruHoradagoda@users.noreply.github.com>
2024-12-02 20:50:43 +0530

    Enhance UI Design for Authentication Screens

--- lib/components/my_button.dart
+++ lib/components/my_button.dart
@@ -3,11 +3,13 @@ import 'package:flutter/material.dart';
 class MyButton extends StatelessWidget {
   final String text;
   final void Function()? onTap;
+  final Color? color; // New color parameter
 
   const MyButton({
     super.key,
     required this.onTap,
     required this.text,
+    this.color, 
   });
 
   @override
@@ -16,7 +18,10 @@ class MyButton extends StatelessWidget {
       onTap: onTap,
       child: Container(
         decoration: BoxDecoration(
-          color: Theme.of(context).colorScheme.primary,
+          color: color ??
+              Theme.of(context)
+                  .colorScheme
+                  .primary, // Use passed color or default to primary
           borderRadius: BorderRadius.circular(12),
         ),
         padding: const EdgeInsets.all(25),
@@ -26,6 +31,7 @@ class MyButton extends StatelessWidget {
             style: const TextStyle(
               fontWeight: FontWeight.bold,
               fontSize: 16,
+              color: Colors.white, // White text for contrast
             ),
           ),
         ),
