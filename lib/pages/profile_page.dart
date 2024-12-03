295ba303cb3611f2702d609a0d4e38431983e01a
AmiruHoradagoda <139224955+AmiruHoradagoda@users.noreply.github.com>
2024-12-03 09:16:50 +0530

    Enhance Profile Page UI with Personalized Greeting, Styled Profile Sections, and Updated Layout

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -88,15 +88,22 @@ class _ProfilePageState extends State<ProfilePage> {
             return Text("Error: ${snapshot.error}");
           } else if (snapshot.hasData) {
             Map<String, dynamic>? user = snapshot.data!.data();
+            String userType = user?['userType'] ??
+                'Customer'; // Check if user is Provider or Customer
+            String greeting = (userType == 'Provider')
+                ? 'Welcome, Service Provider!'
+                : 'Welcome, Valued Customer!';
             return SafeArea(
-              child: Center(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
                 child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     const SizedBox(height: 20),
-                    // Profile Image Section
+                    // Profile Image Section with shadow and rounded borders
                     GestureDetector(
                       onTap: () {
-                        // You can add logic to update the profile image
+                        // Add logic to update the profile image if necessary
                       },
                       child: CircleAvatar(
                         radius: 50,
@@ -108,7 +115,7 @@ class _ProfilePageState extends State<ProfilePage> {
                     ),
                     const SizedBox(height: 20),
                     Text(
-                      user?['username'] ?? 'User Name',
+                      greeting, // Personalized greeting
                       style: TextStyle(
                         fontSize: 24,
                         fontWeight: FontWeight.bold,
@@ -118,26 +125,116 @@ class _ProfilePageState extends State<ProfilePage> {
                             ?.color, // Theme-based text color
                       ),
                     ),
-                    const SizedBox(height: 10),
+                    const SizedBox(height: 8),
+                    Text(
+                      user?['username'] ?? 'User Name',
+                      style: const TextStyle(
+                        fontSize: 22,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.black,
+                      ),
+                    ),
+                    const SizedBox(height: 8),
                     Text(
                       user?['email'] ?? 'Email Address',
                       style: TextStyle(color: Colors.grey[600]),
                     ),
                     const SizedBox(height: 20),
-                    ElevatedButton(
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => EditProfilePage(
-                                userData: user), // Edit Profile Screen
+                    // Bio Section
+                    if (user?['bio'] != null)
+                      Card(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        elevation: 2,
+                        child: Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const Text("Bio",
+                                  style:
+                                      TextStyle(fontWeight: FontWeight.bold)),
+                              const SizedBox(height: 8),
+                              Text(
+                                user?['bio'] ?? 'No bio available',
+                                style: TextStyle(color: Colors.grey[700]),
+                              ),
+                            ],
                           ),
-                        );
-                      },
-                      child: const Text("Edit Profile"),
-                    ),
+                        ),
+                      ),
+                    const SizedBox(height: 20),
+                    // Phone Number Section
+                    if (user?['phone'] != null)
+                      Card(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        elevation: 2,
+                        child: Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Row(
+                            children: [
+                              Icon(Icons.phone,
+                                  color: Theme.of(context).colorScheme.primary),
+                              const SizedBox(width: 10),
+                              Text(user?['phone'] ?? 'Phone Number not set'),
+                            ],
+                          ),
+                        ),
+                      ),
+                    const SizedBox(height: 20),
+                    // Address Section
+                    if (user?['address'] != null)
+                      Card(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        elevation: 2,
+                        child: Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Row(
+                            children: [
+                              Icon(Icons.location_on,
+                                  color: Theme.of(context).colorScheme.primary),
+                              const SizedBox(width: 10),
+                              Text(user?['address'] ?? 'Address not set'),
+                            ],
+                          ),
+                        ),
+                      ),
                     const SizedBox(height: 30),
-                    // Posts Section
+                    // Edit Profile Button with more width
+                    SizedBox(
+                      width:
+                          double.infinity, // Make the button take up full width
+                      child: ElevatedButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) =>
+                                  EditProfilePage(userData: user),
+                            ),
+                          );
+                        },
+                        style: ElevatedButton.styleFrom(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 40, vertical: 16),
+                          backgroundColor:
+                              Theme.of(context).colorScheme.primary,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                        ),
+                        child: const Text("Edit Profile",
+                            style:
+                                TextStyle(fontSize: 16, color: Colors.white)),
+                      ),
+                    ),
+                    const SizedBox(height: 40),
+                    // Posts Section with better title styling
                     const Text("My Posts",
                         style: TextStyle(
                             fontSize: 24, fontWeight: FontWeight.bold)),
@@ -168,21 +265,39 @@ class _ProfilePageState extends State<ProfilePage> {
                                 String? thumbnailUrl =
                                     imageUrls.isNotEmpty ? imageUrls[0] : null;
 
-                                return GestureDetector(
-                                  onTap: () {
-                                    // Navigate to post details
-                                  },
-                                  child: ListTile(
-                                    title: Text(message),
-                                    subtitle: Text(username ?? 'Unknown user'),
-                                    leading: thumbnailUrl != null
-                                        ? Image.network(thumbnailUrl)
-                                        : null,
-                                    trailing: IconButton(
-                                      icon: const Icon(Icons.delete,
-                                          color: Colors.red),
-                                      onPressed: () =>
-                                          _deletePost(context, postId),
+                                return Padding(
+                                  padding:
+                                      const EdgeInsets.symmetric(vertical: 8.0),
+                                  child: Card(
+                                    elevation: 2,
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(12),
+                                    ),
+                                    child: ListTile(
+                                      contentPadding: const EdgeInsets.all(16),
+                                      title: Text(message,
+                                          maxLines: 2,
+                                          overflow: TextOverflow.ellipsis),
+                                      subtitle:
+                                          Text(username ?? 'Unknown user'),
+                                      leading: thumbnailUrl != null
+                                          ? ClipRRect(
+                                              borderRadius:
+                                                  BorderRadius.circular(8),
+                                              child: Image.network(
+                                                thumbnailUrl,
+                                                width: 50,
+                                                height: 50,
+                                                fit: BoxFit.cover,
+                                              ),
+                                            )
+                                          : const Icon(Icons.image, size: 50),
+                                      trailing: IconButton(
+                                        icon: const Icon(Icons.delete,
+                                            color: Colors.red),
+                                        onPressed: () =>
+                                            _deletePost(context, postId),
+                                      ),
                                     ),
                                   ),
                                 );
